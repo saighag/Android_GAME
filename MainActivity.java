9c2c99721490279f80e3d06294996f1fdaf8fb68
SAIRAJ UDAY GHAG <127651023+saighag@users.noreply.github.com>
2023-08-26 20:06:50 +0530

    Add files via upload
    Code for creating "Android Game" using Android Studio.

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sairaj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void startGame(View v) {
+            Log.i("ImageButton", "clicked");
+            Intent intent = new Intent(this,StartGame.class);
+            startActivity(intent);
+            finish();
+        }
+
+}
